5cbc30ddd14ab150c75ce94d1a103474c4d0d881
edwaangu <edwaangu171@outlook.com>
2021-11-19 10:55:59 -0500

    Still working on collisions but almost finished with them

--- BrickBreaker/Ball.cs
+++ BrickBreaker/Ball.cs
@@ -13,9 +13,12 @@ namespace BrickBreaker
         public Image image;
         public Color colour;
 
+        public int collisionTotals;
+
         // Collision related variables
         public string currentBlockCol = "none";
         Rectangle futureRectCol;
+        bool shouldChangeSpeedOnPaddle = false;
 
         public static Random rand = new Random();
 
@@ -30,6 +33,7 @@ namespace BrickBreaker
             ySpeed = _ySpeed;
             size = _ballSize;
             image = _image;
+            collisionTotals = 0;
         }
 
         public void Move()
@@ -37,55 +41,6 @@ namespace BrickBreaker
             // Update position
             x = x + xSpeed;
             y = y + ySpeed;
-
-            // Update position if there was a future collision last frame
-            /*
-            if (currentBlockCol != "none")
-            {
-                if (currentBlockCol == "fromLeft")
-                {
-                    xSpeed = -Math.Abs(xSpeed);
-                    x = Convert.ToInt16(futureRectCol.X - size - 2);
-                }
-                if (currentBlockCol == "fromRight")
-                {
-                    xSpeed = Math.Abs(xSpeed);
-                    x = Convert.ToInt16(futureRectCol.X + futureRectCol.Width + 2);
-                }
-                if (currentBlockCol == "fromUp")
-                {
-                    ySpeed = -Math.Abs(ySpeed);
-                    y = Convert.ToInt16(futureRectCol.Y - size - 2);
-                }
-                if (currentBlockCol == "fromDown")
-                {
-                    ySpeed = Math.Abs(ySpeed);
-                    y = Convert.ToInt16(futureRectCol.Y + futureRectCol.Height + 2);
-                }
-                currentBlockCol = "none";
-            }*/
-        }
-
-        public string Collision(Rectangle r, Rectangle b)
-        {
-            // Check what side the collision is on
-            if(r.IntersectsWith(new Rectangle(b.X, b.Y + b.Height - 5, b.Width, 5)))
-            {
-                return "fromDown";
-            }
-            else if (r.IntersectsWith(new Rectangle(b.X, b.Y, b.Width, 5)))
-            {
-                return "fromUp";
-            }
-            else if (r.IntersectsWith(new Rectangle(b.X, b.Y, 5, b.Height)))
-            {
-                return "fromLeft";
-            }
-            else if (r.IntersectsWith(new Rectangle(b.X + b.Width - 5, b.Y, 5, b.Height)))
-            {
-                return "fromRight";
-            }
-            return "none";
         }
 
         public bool BlockCollision(Block b)
@@ -95,6 +50,75 @@ namespace BrickBreaker
             Rectangle ballRec = new Rectangle(Convert.ToInt16(x), Convert.ToInt16(y), Convert.ToInt16(size), Convert.ToInt16(size));
             Rectangle futureBallRec = new Rectangle(Convert.ToInt16(x + xSpeed), Convert.ToInt16(y + ySpeed), Convert.ToInt16(size), Convert.ToInt16(size));
 
+            if (ballRec.IntersectsWith(blockRec))
+            {
+                double dir = Math.Atan2(ySpeed, xSpeed);
+
+                for (int i = 0; i < 4; i++)
+                {
+                    PointF basePoint  = new PointF(x, y);
+                    PointF subtractPoint = new PointF(0, 0);
+                    if (i == 1)
+                    {
+                        basePoint = new PointF(x + size, y);
+                        subtractPoint = new PointF(size, 0);
+                    }
+                    if (i == 2)
+                    {
+                        basePoint = new PointF(x + size, y + size);
+                        subtractPoint = new PointF(size, size);
+                    }
+                    if (i == 3)
+                    {
+                        basePoint = new PointF(x, y + size);
+                        subtractPoint = new PointF(0, size);
+                    }
+                    PointF currentPoint = new PointF(basePoint.X + Convert.ToSingle(Math.Cos(dir) * size * 3 / 4), basePoint.Y + Convert.ToSingle(Math.Sin(dir) * size * 3 / 4));
+                    PointF point5Ago = new PointF(basePoint.X - (xSpeed * 5), basePoint.Y - (ySpeed * 5));
+
+
+
+                    // Hits left
+                    if (checkIfLineCollision(currentPoint.X, currentPoint.Y, point5Ago.X, point5Ago.Y, blockRec.X, blockRec.Y, blockRec.X, blockRec.Y + blockRec.Height))
+                    {
+                        xSpeed = -Math.Abs(xSpeed);
+                        x = findPointFromLineCollision(currentPoint.X, currentPoint.Y, point5Ago.X, point5Ago.Y, blockRec.X, blockRec.Y, blockRec.X, blockRec.Y + blockRec.Height).X - subtractPoint.X - 2;
+                        collisionTotals++;
+                        break;
+                    }
+
+                    // Hits right
+                    if (checkIfLineCollision(currentPoint.X, currentPoint.Y, point5Ago.X, point5Ago.Y, blockRec.X + blockRec.Width, blockRec.Y, blockRec.X + blockRec.Width, blockRec.Y + blockRec.Height))
+                    {
+                        xSpeed = Math.Abs(xSpeed);
+                        x = findPointFromLineCollision(currentPoint.X, currentPoint.Y, point5Ago.X, point5Ago.Y, blockRec.X + blockRec.Width, blockRec.Y, blockRec.X + blockRec.Width, blockRec.Y + blockRec.Height).X + subtractPoint.X + 2;
+                        collisionTotals++;
+                        break;
+                    }
+
+                    // Hits top
+                    if (checkIfLineCollision(currentPoint.X, currentPoint.Y, point5Ago.X, point5Ago.Y, blockRec.X, blockRec.Y, blockRec.X + blockRec.Width, blockRec.Y))
+                    {
+                        ySpeed = -Math.Abs(ySpeed);
+                        y = findPointFromLineCollision(currentPoint.X, currentPoint.Y, point5Ago.X, point5Ago.Y, blockRec.X, blockRec.Y, blockRec.X + blockRec.Width, blockRec.Y).Y - subtractPoint.Y - 2;
+                        collisionTotals++;
+                        break;
+                    }
+                    // Hits bottom
+                    if (checkIfLineCollision(currentPoint.X, currentPoint.Y, point5Ago.X, point5Ago.Y, blockRec.X, blockRec.Y + blockRec.Height, blockRec.X + blockRec.Width, blockRec.Y + blockRec.Height))
+                    {
+                        ySpeed = Math.Abs(ySpeed);
+                        y = findPointFromLineCollision(currentPoint.X, currentPoint.Y, point5Ago.X, point5Ago.Y, blockRec.X, blockRec.Y + blockRec.Height, blockRec.X + blockRec.Width, blockRec.Y + blockRec.Height).Y + subtractPoint.Y + 2;
+                        collisionTotals++;
+                        break;
+                    }
+                }
+                shouldChangeSpeedOnPaddle = true;
+                return true;
+            }
+            return false;
+
+            /*
             // Check if this block will have a collision with the player next frame
             if(Collision(futureBallRec, blockRec) != "none")
             {
@@ -102,7 +126,45 @@ namespace BrickBreaker
                 futureRectCol = new Rectangle(Convert.ToInt16(x), Convert.ToInt16(y), Convert.ToInt16(size), Convert.ToInt16(size));
             }
 
-            return futureBallRec.IntersectsWith(blockRec);
+            return futureBallRec.IntersectsWith(blockRec);*/
+        }
+
+        public bool checkIfLineCollision(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4)
+        {
+            // Zero length lines
+            if((x1 == x2 && y1 == y2) || (x3 == x4 && y3 == y4))
+            {
+                return false;
+            }
+
+            // No parallel lines
+            float D = (x1 - x2)*(y3 - y4) - (y1 - y2)*(x3 - x4);
+            if(D == 0)
+            {
+                return false;
+            }
+
+
+            float ua = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1));
+            float ub = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1));
+
+            if(ua < 0 || ua > 1 || ub < 0 || ua > 1)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public PointF findPointFromLineCollision(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4)
+        {
+            float ua = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1));
+            float ub = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1));
+
+            float x = x1 + ua * (x2 - x1);
+            float y = y1 + ua * (y2 - y1);
+
+            return new PointF(x, y);
         }
 
         public void PaddleCollision(Paddle p)
@@ -113,13 +175,76 @@ namespace BrickBreaker
             py = y - ySpeed;
             Rectangle paddleRec = new Rectangle(Convert.ToInt16(p.x), Convert.ToInt16(p.y), Convert.ToInt16(p.width), Convert.ToInt16(p.height));
 
+            
+
             if (ballRec.IntersectsWith(paddleRec))
             {
-                if(x + size > p.x && x < p.x + p.width && y + size > p.y && y + size < p.y + p.height / 2)
+                double dir = Math.Atan2(ySpeed, xSpeed);
+
+                for (int i = 0; i < 4; i++)
                 {
-                    y = Convert.ToInt16(p.y - size);
-                    ySpeed = -Math.Abs(ySpeed);
+                    PointF basePoint = new PointF(x, y);
+                    PointF subtractPoint = new PointF(0, 0);
+                    if (i == 1)
+                    {
+                        basePoint = new PointF(x + size, y);
+                        subtractPoint = new PointF(size, 0);
+                    }
+                    if (i == 2)
+                    {
+                        basePoint = new PointF(x + size, y + size);
+                        subtractPoint = new PointF(size, size);
+                    }
+                    if (i == 3)
+                    {
+                        basePoint = new PointF(x, y + size);
+                        subtractPoint = new PointF(0, size);
+                    }
+                    PointF currentPoint = new PointF(basePoint.X + Convert.ToSingle(Math.Cos(dir) * size * 3 / 4), basePoint.Y + Convert.ToSingle(Math.Sin(dir) * size * 3 / 4));
+                    PointF point5Ago = new PointF(basePoint.X - (xSpeed * 5), basePoint.Y - (ySpeed * 5));
+
+                    // Hits top
+                    if (checkIfLineCollision(currentPoint.X, currentPoint.Y, point5Ago.X, point5Ago.Y, paddleRec.X, paddleRec.Y, paddleRec.X + paddleRec.Width, paddleRec.Y))
+                    {
+                        ySpeed = -Math.Abs(ySpeed);
+                        y = findPointFromLineCollision(currentPoint.X, currentPoint.Y, point5Ago.X, point5Ago.Y, paddleRec.X, paddleRec.Y, paddleRec.X + paddleRec.Width, paddleRec.Y).Y - subtractPoint.Y - 5;
+                        collisionTotals++;
+                        if (shouldChangeSpeedOnPaddle)
+                        {
+                            xSpeed += (x - p.x - p.width / 2) / 30;
+                            shouldChangeSpeedOnPaddle = false;
+                        }
+                        break;
+                    }
+                    // Hits bottom
+                    else if (checkIfLineCollision(currentPoint.X, currentPoint.Y, point5Ago.X, point5Ago.Y, paddleRec.X, paddleRec.Y + paddleRec.Height, paddleRec.X + paddleRec.Width, paddleRec.Y + paddleRec.Height))
+                    {
+                        ySpeed = Math.Abs(ySpeed);
+                        y = findPointFromLineCollision(currentPoint.X, currentPoint.Y, point5Ago.X, point5Ago.Y, paddleRec.X, paddleRec.Y + paddleRec.Height, paddleRec.X + paddleRec.Width, paddleRec.Y + paddleRec.Height).Y + subtractPoint.Y + 5;
+                        collisionTotals++;
+                        //xSpeed += (x - p.x - p.width / 2) / 15;
+                        break;
+                    }
+
+                    // Hits left
+                    if (checkIfLineCollision(currentPoint.X, currentPoint.Y, point5Ago.X, point5Ago.Y, paddleRec.X, paddleRec.Y, paddleRec.X, paddleRec.Y + paddleRec.Height))
+                    {
+                        xSpeed = -Math.Abs(xSpeed);
+                        x = findPointFromLineCollision(currentPoint.X, currentPoint.Y, point5Ago.X, point5Ago.Y, paddleRec.X, paddleRec.Y, paddleRec.X, paddleRec.Y + paddleRec.Height).X - subtractPoint.X - 5;
+                        collisionTotals++;
+                        break;
+                    }
+
+                    // Hits right
+                    else if (checkIfLineCollision(currentPoint.X, currentPoint.Y, point5Ago.X, point5Ago.Y, paddleRec.X + paddleRec.Width, paddleRec.Y, paddleRec.X + paddleRec.Width, paddleRec.Y + paddleRec.Height))
+                    {
+                        xSpeed = Math.Abs(xSpeed);
+                        x = findPointFromLineCollision(currentPoint.X, currentPoint.Y, point5Ago.X, point5Ago.Y, paddleRec.X + paddleRec.Width, paddleRec.Y, paddleRec.X + paddleRec.Width, paddleRec.Y + paddleRec.Height).X - subtractPoint.X + 5;
+                        collisionTotals++;
+                        break;
+                    }
                 }
+
             }
 
             /*
@@ -150,7 +275,8 @@ namespace BrickBreaker
                         xSpeed += (x - p.x - p.width / 2) / 15;
                     }
                 }
-            }*/
+            }
+            */
         }
 
         public void WallCollision(UserControl UC)
@@ -159,7 +285,8 @@ namespace BrickBreaker
             if (x <= 0)
             {
                 xSpeed *= -1;
-                x = 1;
+                x = 1; 
+                shouldChangeSpeedOnPaddle = true;
             }
 
             // Collision with right wall
@@ -167,6 +294,7 @@ namespace BrickBreaker
             {
                 xSpeed *= -1;
                 x = UC.Width - size - 1;
+                shouldChangeSpeedOnPaddle = true;
             }
 
             // Collision with top wall
@@ -174,6 +302,7 @@ namespace BrickBreaker
             {
                 ySpeed *= -1;
                 y = 3;
+                shouldChangeSpeedOnPaddle = true;
             }
         }
 
@@ -185,6 +314,7 @@ namespace BrickBreaker
             if (y >= UC.Height)
             {
                 didCollide = true;
+                shouldChangeSpeedOnPaddle = false;
             }
 
             return didCollide;
