2ac8cd65c84387cc098c9b1e1c5d2df24d22d960
edwaangu <edwaangu171@outlook.com>
2021-12-02 09:12:57 -0500

    prob

--- BrickBreaker/Properties/Resources.Designer.cs
+++ BrickBreaker/Properties/Resources.Designer.cs
@@ -133,6 +133,16 @@ namespace BrickBreaker.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        public static System.Drawing.Bitmap BACK {
+            get {
+                object obj = ResourceManager.GetObject("BACK", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         public static System.Drawing.Bitmap BALL {
             get {
                 object obj = ResourceManager.GetObject("BALL", resourceCulture);
